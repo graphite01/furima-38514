073709c38530e5366c67e745c0799c0613a31267
graphite01 <kirito10082279@gmail.com>
2022-10-06 10:47:37 +0900

    userモデルのバリデーション設定

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name_chinese, presence: true
+  validates :first_name_chinese, presence: true
+  validates :last_name_katakana, presence: true
+  validates :first_name_katakana, presence: true
+  validates :birth_day, presence: true
+
+end
