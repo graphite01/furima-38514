c325dcd5c874b8f2847952747dba3ba2770679b7
graphite01 <kirito10082279@gmail.com>
2022-10-08 14:11:54 +0900

    出品画面エラー表示実装

--- app/models/commodity_exhibition.rb
+++ app/models/commodity_exhibition.rb
@@ -2,7 +2,7 @@ class CommodityExhibition < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :explanation
-    validates :selling_price
+    validates :image
   end
 
   with_options presence: true, numericality: { other_than: 1 } do
@@ -11,6 +11,10 @@ class CommodityExhibition < ApplicationRecord
     validates :delivery_charge_id
     validates :prefecture_id
     validates :days_to_ship_id
+  end
+
+  with_options presence: true, format: { with: /\A[0-9]+\z/, message: ' is invalid. Input half-width characters'}, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: ' is out of setting range'} do
+    validates :selling_price, presence: { message: "cant't be blank" }
   end
 
   belongs_to :user
