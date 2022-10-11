a84f3ed0ef8d49754c9e738b90488feb1367770d
graphite01 <kirito10082279@gmail.com>
2022-10-12 08:51:59 +0900

    product_purchasesコントローラー作成

---
+++ app/controllers/product_purchases_controller.rb
@@ -0,0 +1,7 @@
+class ProductPurchasesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
---
+++ spec/requests/product_purchases_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "ProductPurchases", type: :request do
+
+end
