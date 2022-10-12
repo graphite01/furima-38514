e52f057d003db4e14b45556ae8b0624300ff329a
graphite01 <kirito10082279@gmail.com>
2022-10-12 13:26:17 +0900

    購入機能（雛形）作成

--- app/controllers/purchase_historys_controller.rb
+++ app/controllers/purchase_historys_controller.rb
@@ -5,19 +5,19 @@ class PurchaseHistorysController < ApplicationController
   end
 
   def create
-    binding.pry
+    @commodity_exhibition = CommodityExhibition.find(params[:commodity_exhibition_id])
     @product_history = ProductHistorys.new(product_params)
     if @product_history.valid?
       @product_history.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def product_params
-    params.require(:product_history).permit(:post_code, :municipalities, :address, :building_name, :phone_number, :prefecture_id)
+    params.require(:product_historys).permit(:post_code, :municipalities, :address, :building_name, :phone_number, :prefecture_id)
   end
 end
