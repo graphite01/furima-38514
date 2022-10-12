852f95304bd0dcc56c3f709807fe27664fb1d699
graphite01 <kirito10082279@gmail.com>
2022-10-12 09:00:27 +0900

    product_purchaseのindexビューファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'commodity_exhibitions#index'
   resources :commodity_exhibitions
+  resources :product_purchases, only: [:index, :create]
 end
